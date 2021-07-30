1f2af023a126d73cbc4093a5f9b83905f42b8279
kishanInventam <kishanyadav.inventam@gmail.com>
2021-07-30 13:24:54 +0530

    change port

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ connection.once("open", ()=>{
 const userRoutes = require("./routes/user.js");
 app.use(express.json())
 app.use("/user", userRoutes);
-const port  = process.env.port||5000;
+const port  = process.env.PORT||5000;
 app.route("/").get((req, res) =>res.json("your first rest api and this will display"));
 app.listen(port, ()=>console.log("your server is running on port number 5000"));
